9959de2f1899ffe7b0ce24c309482de45f17cecd
Jonathan Imlah <jimlah42@gmail.com>
2022-12-22 12:47:33 +1000

    added proper date handling, temp rm slp files causes tests to fail

--- src/replays/loadFile.ts
+++ src/replays/loadFile.ts
@@ -1,5 +1,10 @@
+/* Code based off https://github.com/project-slippi/slippi-launcher/blob/main/src/replays/loadFile.ts
+  from project-slippi/slippi-launcher
+*/
+
 import type { MetadataType } from "@slippi/slippi-js";
 import { SlippiGame } from "@slippi/slippi-js";
+import * as fs from "fs-extra";
 import _ from "lodash";
 import moment from "moment";
 import path from "path";
@@ -19,7 +24,8 @@ export async function loadFile(fullPath: string): Promise<GameStats | null> {
   const metadata: MetadataType | null = game.getMetadata();
 
   if (metadata === null) {
-    return null;
+    throw new Error("no metadata");
+    // return null;
   }
 
   const duration = Math.round(metadata.lastFrame! / 60);
@@ -29,6 +35,10 @@ export async function loadFile(fullPath: string): Promise<GameStats | null> {
   }
 
   const settings = game.getSettings()!;
+  if (settings.players.length !== 2) {
+    console.log("not 1v1");
+    return null;
+  }
   const stats = game.getStats()!;
   const gameEnd = game.getGameEnd()!;
   const players = metadata.players;
@@ -40,13 +50,12 @@ export async function loadFile(fullPath: string): Promise<GameStats | null> {
 
   const placements = gameEnd.placements!;
   const didWin = placements[0].playerIndex == playerId ? 1 : 0;
-  //TODO extend dateTime to catch if file has been renamed
-  const asMoment = moment(metadata.startAt);
-  let dateTime;
-  if (asMoment.isValid()) {
-    dateTime = asMoment.local();
-  } else {
-    throw new Error("Failed to read date");
+
+  const dateTime = await fileToDateTime(metadata ? metadata.startAt : null, filename, fullPath);
+
+  if (!dateTime) {
+    console.log("Failed to read time");
+    return null;
   }
 
   const gameStats: GameStats = {
@@ -72,4 +81,45 @@ export async function loadFile(fullPath: string): Promise<GameStats | null> {
     FileName: filename,
   };
   return gameStats;
+}
+
+function convertToDateTime(dateTimeString: moment.MomentInput): moment.Moment | null {
+  const asMoment = moment(dateTimeString);
+  if (asMoment.isValid()) {
+    return asMoment.local();
+  }
+  return null;
+}
+
+async function fileToDateTime(
+  dateTimeString: string | undefined | null,
+  fileName: string,
+  fullPath: string,
+): Promise<moment.Moment | null> {
+  let startAt = convertToDateTime(dateTimeString);
+  if (startAt) {
+    return startAt;
+  }
+
+  startAt = filenameToDateTime(fileName);
+  if (startAt) {
+    return startAt;
+  }
+
+  const { birthtime } = await fs.stat(fullPath);
+  startAt = convertToDateTime(birthtime);
+
+  return startAt;
+}
+
+function filenameToDateTime(filename: string): moment.Moment | null {
+  const timeReg = /\d{8}T\d{6}/g;
+  const filenameTime = filename.match(timeReg);
+
+  if (filenameTime === null) {
+    return null;
+  }
+
+  const time = moment(filenameTime[0]).local();
+  return time;
 }
