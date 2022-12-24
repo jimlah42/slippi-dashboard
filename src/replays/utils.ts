d365ec860bb22dad1a2067e5ecedccabf9c3c401
Jonathan Imlah <jimlah42@gmail.com>
2022-12-24 22:39:46 +1000

    refactor: merged replay utils into one file

--- src/replays/utils.ts
+++ src/replays/utils.ts
@@ -35,3 +35,16 @@ export function getCharNameByIndex(index: number): string {
     return "Unknown";
   }
 }
+
+const STAGE_NAMES = {
+  2: "Fountain of Dreams",
+  3: "Pokémon Stadium",
+  8: "Yoshi's Story",
+  28: "Dream Land N64",
+  31: "Battlefield",
+  32: "Final Destination",
+};
+
+export function getStageNameByIndex(index: number): string {
+  return STAGE_NAMES[index];
+}
